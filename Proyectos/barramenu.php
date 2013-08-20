17dee1265b4e431b20ae35358dce3be9fa2536b6
Mario Alvarado <Mario Alvarado@blackmaab>
2013-08-20 00:36:06 -0600

    Creacion de los mantenimientos del formulario entradas

--- Proyectos/barramenu.php
+++ Proyectos/barramenu.php
@@ -9,15 +9,11 @@
                     <li><a href="RegistrarUsuarios.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Usuarios del sistema";}else{ echo "User/System user"; }?></span></a></li>
                 </ul>
             </li>
-            <li><a href="RegistrarTipoProyecto.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Tipo de proyecto";}else{ echo "Project Type"; }?></span></a></li>
-            <li><a href="AgregarProyectos.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Proyecto";}else{ echo "Project"; }?></span></a></li>
-            <li><a href="AgregarEntrada.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Nueva entrada";}else{ echo "New entry"; }?></span></a></li>
-            <li><a href="AgregarEntrada.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Nuevo reporte";}else{ echo "New report"; }?></span></a></li>
+           
 
             <li><a href="RegistrarTipoProyecto.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Tipo de proyecto";}else{ echo "Project Type"; }?></span></a></li>
             <li><a href="RegistrarProyecto.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Proyecto";}else{ echo "Project"; }?></span></a></li>
             <li><a href="RegistrarPersonalProyecto.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Registrar Personal a Proyecto";}else{ echo "Project Personnel Register"; }?></span></a></li>
-            <li><a href="AgregarEntrada.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Nueva entrada";}else{ echo "New entry"; }?></span></a></li>
            <li><a href="RegistroEntradas.php"><span><?php if($_SESSION["idioma"]=="es"){echo "Nuevo reporte";}else{ echo "New report"; }?></span></a></li>
             <li><a href="#"><?php if($_SESSION["idioma"]=="es"){echo "Cat&aacute;logos";}else{ echo "Catalogs"; }?></a>
                 <ul>
